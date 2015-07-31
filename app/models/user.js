fa0810f6fa68f9aa05f11d3dac18840fd182a401
Hack Reactor Students <accounts@hackreactor.com>
2015-07-30 19:59:11 -0700

    Finished Basic Reqs

--- app/models/user.js
+++ app/models/user.js
@@ -2,24 +2,30 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
+var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+
+var userSchema = new mongoose.Schema({ 
+  username: {type: String, unique: true}, 
+  password: String}
+);
+
+userSchema.pre('save', function(next){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
     });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
 });
+
+
+var User = mongoose.model('User', userSchema);
+
+User.prototype.comparePassword = function(attemptedPassword, callback){
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+};
 
 module.exports = User;
